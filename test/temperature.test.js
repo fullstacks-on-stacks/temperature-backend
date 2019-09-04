d4d5f376ac3d656238d59577d3a7109542240df9
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-09-04 16:09:20 -0700

    get all temperatures

---
+++ test/temperature.test.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Temperature = require('../lib/models/Temperature');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can get all the temperatures', async() => {
+    const temp = await Temperature.create([
+      { temperature: 25 },
+      { temperature: 50 }
+    ]);
+    return request(app)
+      .get('/')
+      .then(res => {
+        const tempJSON = JSON.parse(JSON.stringify(temp));
+        tempJSON.forEach(temp => {
+          expect(res.body).toContainEqual({
+            temperature: temp.temperature,
+            _id: temp._id
+          });
+        });  
+      });
+  });
+});
